cce95eb6ff918f20ebb9e1a343b73fcfd36350a6
codelin <494020937@qq.com>
2021-09-05 21:31:50 +0800

    composer.json require php>=8.0
    Signed-off-by: codelin <494020937@qq.com>

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -15,12 +15,9 @@ namespace App\Controller;
 use App\Middleware\JwtMiddleware;
 use App\Model\Member;
 use App\Model\User;
-use Hyperf\DbConnection\Db;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Annotation\Middleware;
-use Hyperf\Utils\Arr;
-use Hyperf\Utils\Context;
 use Qbhy\HyperfAuth\AuthManager;
 
 #[AutoController]
@@ -32,18 +29,18 @@ class IndexController extends AbstractController
     #[Inject]
     protected AuthManager $auth;
 
-    public function index()
+    public function index(): array
     {
         $user   = $this->request->input('user', 'Hyperf111');
         $method = $this->request->getMethod();
 
         return [
             'method'  => $method,
-            'message' => "Hello {$user}.",
+            'message' => "Hello $user.",
         ];
     }
 
-    public function add(\App\Request\Member $request)
+    public function add(\App\Request\Member $request): array
     {
         $request->scene('test')->validateResolved();
         return $request->all();
@@ -54,11 +51,7 @@ class IndexController extends AbstractController
         /** @var User $user */
         $user = User::query()->where('id', 29928)->first();
         $user->setId($user['id']);
-        $token = $this->auth->login($user);
-        $jwt   = $this->auth->guard('jwt')->getJwtManager()->parse($token);
-        $uid   = $jwt->getPayload();
-        var_dump($uid);
-        return $token;
+        return $this->auth->login($user);
     }
 
     #[Middleware(JwtMiddleware::class)]
