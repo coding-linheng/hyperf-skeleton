3b57bfe611f418ebd684d7c58759a4c346254f12
willion <764103987@qq.com>
2021-09-24 00:12:48 +0800

    增加风控相关文件

--- app/Common/Rcp.php
+++ app/Common/Rcp.php
@@ -7,6 +7,7 @@ namespace App\Common;
 use App\Constants\ErrorCode;
 use App\Exception\BusinessException;
 use Hyperf\Redis\RedisProxy;
+use phpseclib3\Math\BigInteger;
 use Psr\Http\Message\ServerRequestInterface;
 
 
@@ -21,15 +22,48 @@ class Rcp
 
     protected array $user;   //当前用户，可以为空
 
+    protected BigInteger $userCode;   //当前用户编码，唯一标记即可，默认使用id,可以为空
+
     protected array $configs;  //配置数组
 
+    protected string $uri;
+
+    protected string $ip;
+
+   /********************* 基本风控 **************************************/
+
+    protected int $RCP_USER_DEFAULT_LIMIT=10000; //用户每天访问默认限制次数
+
+    protected int $RCP_IP_DEFAULT_LIMIT=10000; //每个IP每天访问默认限制次数
+
+    protected int $RCP_IP_URI_DEFAULT_LIMIT=2000; //每个IP每天访问某个具体URI默认限制次数
+
+    protected int $RCP_USER_URI_DEFAULT_LIMIT=500; // 用户每天访问某个具体URI默认限制次数
+
     const RCP_USER="RCP_USER_Z"; //用户访问次数，后缀Z表示使用Zset
+
     const RCP_IP="RCP_IP_Z"; //IP次数，使用Zset
-    const RCP_URI="RCP_IP_Z"; //IP次数，使用Zset
+
+    const RCP_USER_AGENT="RCP_USER_AGENT_Z"; //USER_AGENT次数，使用Zset
+
+    const RCP_IP_URI="RCP_IP_URI_Z"; //URI次数，使用Zset
+
     const RCP_USER_URI="RCP_USER_URI_Z"; //用户-uri 次数，使用Zset
 
+   /********************* 黑名单  **************************************/
 
+    const RCP_DAY_BLACK_IP="RCP_DAY_BLACK_IP_Z"; //用户当天临时IP黑名单,连续3次进入该名单则被拉入永久封禁
 
+    const RCP_BLACK_IP="RCP_BLACK_IP_Z"; //永久用户IP黑名单
+
+    const RCP_DAY_BLACK_USER="RCP_DAY_BLACK_IP_Z"; //用户当天临时黑名单,连续3次进入该名单则被拉入永久封禁
+
+    const RCP_BLACK_USER="RCP_BLACK_IP_Z"; //永久用户黑名单
+
+   /********************* 日志入库  **************************************/
+
+    //请求参数日志列表，经过整理后入库，后续看日志量决定是否存入日志分析库
+    const RCP_LOG_DETAIL_LIST="RCP_USER_URI_Z";
 
     public function __construct()
     {
@@ -64,16 +98,61 @@ class Rcp
     {
         $this->request = $request;
         $this->user    = $user;
+        $this->uri = $this->request->getUri()->getPath();
+        $this->ip = get_client_ip();
+        $this->userCode=$user['id']??0;
+
+        //判断是否属于黑名单，如果属于直接报错
+        if (!$this->checkRcpBlackList()) {
+          throw new BusinessException(ErrorCode::SERVER_RCP_ERROR,"您已限制访问");
+        }
 
         //防CC，防爬虫等公用策略
         if (!$this->checkRcp()) {
-            throw new BusinessException(ErrorCode::SERVER_RCP_ERROR);
+            throw new BusinessException(ErrorCode::SERVER_RCP_ERROR,"您已暂时限制部分访问");
         }
         //定制风控策略
         if ($this->checkSpecialRcp()) {
             throw new BusinessException(ErrorCode::SERVER_RCP_ERROR);
         }
         return true;
+    }
+
+    /**
+     * 检查风控是否触发.公共策略部分检查.
+     */
+    private function checkRcpBlackList(): bool
+    {
+
+      //用户当天临时IP黑名单,连续3次进入该名单则被拉入永久封禁
+      $isDayBlackIp= $this->redis->zScore(self::RCP_DAY_BLACK_IP,$this->ip);
+      if($isDayBlackIp){
+        return false;
+      }
+
+      //永久用户IP黑名单
+      $isBlackIp= $this->redis->zScore(self::RCP_BLACK_IP,$this->ip);
+      if($isBlackIp && $isBlackIp>=3){
+        return false;
+      }
+
+      if(empty($this->userCode)){
+        return true;
+      }
+
+      //用户当天临时黑名单,连续3次进入该名单则被拉入永久封禁
+      $isDayBlackUser= $this->redis->zScore(self::RCP_DAY_BLACK_USER,$this->userCode);
+      if($isDayBlackUser){
+        return false;
+      }
+
+      //永久用户黑名单
+      $isBlackUser= $this->redis->zScore(self::RCP_BLACK_USER,$this->userCode);
+      if($isBlackUser && $isBlackUser>=3){
+        return false;
+      }
+
+      return true;
     }
 
     /**
@@ -103,10 +182,51 @@ class Rcp
      */
     private function checkRcpByUri(): bool
     {
+        $this->RCP_IP_URI_DEFAULT_LIMIT=$this->configs["limit_count"]["uri_day_limit"]??$this->RCP_IP_URI_DEFAULT_LIMIT;
+
         //检查当前ip，账号一分钟内访问该Url次数
+        if($this->redis->exists(self::RCP_IP_URI.date("YmdHi"))){
+          //当前次数+1
+          $mTimes= $this->redis->zIncrBy(self::RCP_IP_URI.date("YmdHi"),1,$this->uri."-".$this->ip);
+          if($mTimes>=intval($this->RCP_IP_URI_DEFAULT_LIMIT*20/(24*60))){
+             return false;
+          }
+        }else{
+          //新增key
+          $this->redis->zAdd(self::RCP_IP_URI.date("YmdHi"),1,$this->uri."-".$this->ip);
+          //设置过期时间
+          $this->redis->expire(self::RCP_IP_URI.date("YmdHi"),60);
+        }
+
         //检查当前ip，账号一小时内访问该Url次数
+        if($this->redis->exists(self::RCP_IP_URI.date("YmdH"))){
+          //当前次数+1
+          $hTimes=$this->redis->zIncrBy(self::RCP_IP_URI.date("YmdH"),1,$this->uri."-".$this->ip);
+          if($hTimes>=intval($this->RCP_IP_URI_DEFAULT_LIMIT*5/(24))){
+            return false;
+          }
+        }else{
+          //新增key
+          $this->redis->zAdd(self::RCP_IP_URI.date("YmdH"),1,$this->uri."-".$this->ip);
+          //设置过期时间
+          $this->redis->expire(self::RCP_IP_URI.date("YmdH"),3600);
+        }
+
         //检查当前ip，账号一天内访问该Url次数
-        //检查当前ip，账号总访问该Url次数
+        if($this->redis->exists(self::RCP_IP_URI.date("Ymd"))){
+          //当前次数+1
+          $dayTimes = $this->redis->zIncrBy(self::RCP_IP_URI.date("Ymd"),1,$this->uri."-".$this->ip);
+        }else{
+          //新增key
+          $dayTimes =1;
+          $this->redis->zAdd(self::RCP_IP_URI.date("Ymd"),1,$this->uri."-".$this->ip);
+          //设置过期时间
+          $this->redis->expire(self::RCP_IP_URI.date("Ymd"),3600*24);
+        }
+        if($dayTimes>$this->RCP_IP_URI_DEFAULT_LIMIT){
+          $this->redis->zIncrBy(self::RCP_DAY_BLACK_IP,1,$this->ip);
+          $this->redis->zIncrBy(self::RCP_BLACK_IP,1,$this->ip);
+        }
 
         return true;
     }
