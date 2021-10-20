6f6e0d9c50294efe532c30c50f24000b8e3aa69e
kehao <123456@qq.com>
2021-10-20 15:38:31 +0800

    专辑详情列表

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -15,8 +15,8 @@ use App\Middleware\RequestMiddleware;
 return [
     // 下面的 http 字符串对应 config/autoload/server.php 内每个 server 的 name 属性对应的值，意味着对应的中间件配置仅应用在该 Server 中
     'http' => [
-        RequestMiddleware::class,
         App\Middleware\CorsMiddleware::class, // 跨域处理
+        RequestMiddleware::class,
         // 数组内配置您的全局中间件，顺序根据该数组的顺序
 //        Hyperf\Validation\Middleware\ValidationMiddleware::class,
         // 这里隐藏了其它中间件
